df51a5e28a7d95d54b7464c68b1c03381bb6f1ed
yorn <git@yorn.priv.no>
2013-04-19 12:21:36 +0200

    Working showcase for Taxi
    Taxisystem will draw a square on the map

--- src/ttm4115/taxi/Taxi.java
+++ src/ttm4115/taxi/Taxi.java
@@ -1,7 +1,48 @@
 package ttm4115.taxi;
 
+import java.util.UUID;
+
+import com.bitreactive.library.android.maps.model.MapUpdate;
+import com.bitreactive.library.android.maps.model.Marker;
+import com.bitreactive.library.android.maps.model.Position;
+import com.bitreactive.library.mqtt.mqtt.MQTT;
+import com.bitreactive.library.mqtt.mqtt.MQTT.Message;
+
 import no.ntnu.item.arctis.runtime.Block;
 
 public class Taxi extends Block {
+
+	public void configureMQTT() {
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Taxi Simulator");
+		setProperty(MQTT.P_MQTT_CLIENT_ID, UUID.randomUUID().toString().substring(0, 20));
+		setProperty(MQTT.P_MQTT_PORT, "1883");
+		setProperty(MQTT.P_MQTT_SERVER, "broker.mqttdashboard.com");
+		setProperty(MQTT.P_MQTT_TOPIC_PUBLISH, "generic-map-ui-group05");
+		setProperty(MQTT.P_MQTT_TOPIC_SUBSCRIBE, "generic-map-ui-group05");
+	}
+
+	public MapUpdate randomMapUpdate() {
+		MapUpdate update = new MapUpdate();
+		Marker marker = Marker.createMarker("taxi1");
+		marker.position(new Position(0,0));
+		update.addMarker(marker);
+		
+		marker = Marker.createMarker("taxi2");
+		marker.position(new Position(-10,0));
+		update.addMarker(marker);
+		
+		marker = Marker.createMarker("taxi3");
+		marker.position(new Position(0,-10));
+		update.addMarker(marker);
+		
+		marker = Marker.createMarker("taxi4");
+		marker.position(new Position(-10,-10));
+		update.addMarker(marker);
+		return update;
+	}
+
+	public Message stringToMessage(String string) {
+		return new Message(string.getBytes());
+	}
 
 }
