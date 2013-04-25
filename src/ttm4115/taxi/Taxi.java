8feeed3d7a93b88390cdb3c48a8eeda32ba61b81
yorn <git@yorn.priv.no>
2013-04-25 14:08:19 +0200

    Implemented most of 2.2

--- src/ttm4115/taxi/Taxi.java
+++ src/ttm4115/taxi/Taxi.java
@@ -1,48 +1,48 @@
 package ttm4115.taxi;
 
-import java.util.UUID;
-
-import com.bitreactive.library.android.maps.model.MapUpdate;
-import com.bitreactive.library.android.maps.model.Marker;
 import com.bitreactive.library.android.maps.model.Position;
+import com.bitreactive.library.googleapis.latitude.LocationType;
+import com.bitreactive.library.googleapis.latitude.SingleLocationData;
 import com.bitreactive.library.mqtt.mqtt.MQTT;
 import com.bitreactive.library.mqtt.mqtt.MQTT.Message;
 
 import no.ntnu.item.arctis.runtime.Block;
+import no.ntnu.item.ttm4115.library.routeplanner.routeplanner.Journey;
 
 public class Taxi extends Block {
 
-	public void configureMQTT() {
+	int step = 0;
+	int taxiId;
+
+	public static String getAlias(int taxiId) {
+		return "Taxi "+taxiId;
+	}
+
+	public void construct(int taxiId) {
+		this.taxiId = taxiId;
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Taxi Simulator");
-		setProperty(MQTT.P_MQTT_CLIENT_ID, UUID.randomUUID().toString().substring(0, 20));
+		setProperty(MQTT.P_MQTT_CLIENT_ID, getAlias(taxiId));
 		setProperty(MQTT.P_MQTT_PORT, "1883");
 		setProperty(MQTT.P_MQTT_SERVER, "broker.mqttdashboard.com");
 		setProperty(MQTT.P_MQTT_TOPIC_PUBLISH, "generic-map-ui-group05");
 		setProperty(MQTT.P_MQTT_TOPIC_SUBSCRIBE, "generic-map-ui-group05");
 	}
 
-	public MapUpdate randomMapUpdate() {
-		MapUpdate update = new MapUpdate();
-		Marker marker = Marker.createMarker("taxi1");
-		marker.position(new Position(0,0));
-		update.addMarker(marker);
-		
-		marker = Marker.createMarker("taxi2");
-		marker.position(new Position(-10,0));
-		update.addMarker(marker);
-		
-		marker = Marker.createMarker("taxi3");
-		marker.position(new Position(0,-10));
-		update.addMarker(marker);
-		
-		marker = Marker.createMarker("taxi4");
-		marker.position(new Position(-10,-10));
-		update.addMarker(marker);
-		return update;
+	public Message stringToMessage(String string) {
+		System.out.println(string);
+		return new Message(string.getBytes());
 	}
 
-	public Message stringToMessage(String string) {
-		return new Message(string.getBytes());
+	public Journey createJourney(SingleLocationData start, Position destination) {
+		return new Journey(
+				start.getData().getLatitude()+","+start.getData().getLongitude(),
+				destination.getLatitude()+","+destination.getLongitude(),
+				getAlias(taxiId)
+			);
+	}
+
+	public SingleLocationData getMoholtLocation() {
+		return new SingleLocationData(new LocationType(63.422528, 10.394729));
 	}
 
 }
--- src/ttm4115/taxisystem/component/Component.java
+++ src/ttm4115/taxisystem/component/Component.java
@@ -1,7 +1,19 @@
 package ttm4115.taxisystem.component;
 
+import com.bitreactive.library.android.maps.model.Position;
+
 import no.ntnu.item.arctis.runtime.Block;
 
 public class Component extends Block {
+
+	int taxiId;
+
+	public int generateTaxiId() {
+		return taxiId++;
+	}
+
+	public Position getStudenterSamfundetLocation() {
+		return new Position(63.422528, 10.394729);
+	}
 
 }
